4fdd461ad63fb5798fcecd90ddc9994f6278af0f
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-10-02 12:52:43 +0200

    Increase available time for heavy download

--- arduino/shell.nix
+++ arduino/shell.nix
@@ -16,6 +16,7 @@ pkgs.mkShell {
 		arduino-cli config init
 		arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 		arduino-cli core update-index
+		arduino-cli config set network.connection_timeout 300s
 		arduino-cli core install esp32:esp32
 		sudo chmod a+rw /dev/ttyUSB0
 		chmod +x Run.sh

993c6c36f1bdfbad08150ea689f6b03bcb96c0ac
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-11-03 10:03:59 +0100

    Added PyQt5 template

---
+++ PyQt5/shell.nix
@@ -0,0 +1,35 @@
+#shell.nix
+# If the executable is not main.py, change the Run command with :%s/main.py/<path-to-executable>, then delete .nix-bin/ before restarting this shell
+# Or modify the Run command directly in .nix-bin/Run
+with import <nixpkgs> {}; 
+
+pkgs.mkShell {
+	name = "pyqt-shell";
+
+  # These lines use nix-ld to help python's terrible packaging and distribution system to find it's damn dependencies on NixOS
+  # https://www.crescentro.se/posts/python-nixos/
+	NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    stdenv.cc.cc # libstdc++
+    zlib # libz (for numpy)
+  ];
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+
+  # uv init or uv init --lib or uv init --bare
+  # uv add <package>
+  # uv pip install -r requirements.txt
+	packages = with pkgs; [
+		uv 	
+		qt5.full
+		qtcreator
+  ];
+
+	shellHook = ''
+		export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+
+    if [ ! -d ".venv" ]; then
+      uv venv
+    fi
+
+    source .venv/bin/activate
+	'';
+}

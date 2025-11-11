516365eb884d82cda88d708442f5c344e1302222
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-11-11 01:32:48 +0100

    Added pyqtwebengine and direnv to PyQt5 shell

--- PyQt5/shell.nix
+++ PyQt5/shell.nix
@@ -1,6 +1,4 @@
 #shell.nix
-# If the executable is not main.py, change the Run command with :%s/main.py/<path-to-executable>, then delete .nix-bin/ before restarting this shell
-# Or modify the Run command directly in .nix-bin/Run
 with import <nixpkgs> {}; 
 
 pkgs.mkShell {
@@ -9,16 +7,30 @@ pkgs.mkShell {
   # These lines use nix-ld to help python's terrible packaging and distribution system to find it's damn dependencies on NixOS
   # https://www.crescentro.se/posts/python-nixos/
 	NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-    stdenv.cc.cc # libstdc++
-    zlib # libz (for numpy)
-		glib # libgthread-2.0.so.0
-		libGL # libGL.so.1
+		alsa-lib # libasound.so.2
+		dbus # libdbus-1.so.3
+		expat # libexpat.so.1
 		fontconfig # libfontconfig.so.1
 		freetype # libfreetype.so.6
+		glib # libgthread-2.0.so.0
+		krb5 # libgssapi_krb5.so.2
+		libGL # libGL.so.1
+		libudev0-shim # libudev.so.0
 		libxkbcommon # libxkbcommon.so.0
-		dbus # libdbus-1.so.3
+		nss # libsmime3.so
+		nspr # libplds4.so
+    stdenv.cc.cc # libstdc++
 		xorg.libX11 # libX11.so.6
+		xorg.libxcb # libxcb.so.1
+		xorg.libXcomposite # libXcomposite.so.1
+		xorg.libXdamage # libXdamage.so.1
+		xorg.libXext # libXext.so.6
+		xorg.libXfixes # libXfixes.so.3
+		xorg.libXrandr # libXrandr.so.2
+		xorg.libXrender # libXrender.so.1
+		xorg.libXtst # libXtst.so.6
 		wayland # libwayland-client.so.0
+    zlib # libz (for numpy)
 	];
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
 
@@ -35,14 +47,11 @@ pkgs.mkShell {
 		export QT_DEBUG_PLUGINS=1
 		export QT_QPA_PLATFORM=wayland
 
-		if [ ! -d ".envrc" ]; then
-			echo "use nix" > .envrc
-			direnv allow
-		fi
-
-    if [ ! -d ".venv" ]; then
-      uv venv
+    if [ -d ".venv" ]; then
+			rm -rf .venv
     fi
+		uv venv
+		uv pip install -r requirements.txt
 
     source .venv/bin/activate
 	'';

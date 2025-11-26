57d29ec3fc47494a9b19c1b6c16fdb7587da357f
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-11-26 13:27:23 +0100

    Added extra libs, added SPIFFS support

--- arduino/Run.sh
+++ arduino/Run.sh
@@ -13,3 +13,17 @@ PORT="/dev/ttyUSB0"
 # Compile and upload
 arduino-cli board attach -p "$PORT" -b "$FQBN" "$SKETCH_PATH/$SKETCH_PATH.ino" 
 arduino-cli compile --fqbn "$FQBN" "$SKETCH_PATH" --upload -p "$PORT"
+
+if [ -d "$SKETCH_PATH/data" ]; then
+	~/.arduino15/packages/esp32/tools/mkspiffs/0.2.3/mkspiffs \
+					-c $SKETCH_PATH/data \
+					-b 4096 \
+					-p 256 \
+					-s 0x160000 \
+					spiffs.bin
+	~/.arduino15/packages/esp32/tools/esptool_py/5.1.0/esptool \
+					--chip esp32 \
+					--port "$PORT" \
+					--baud 921600 \
+					write_flash 0x290000 spiffs.bin
+fi
--- arduino/shell.nix
+++ arduino/shell.nix
@@ -1,4 +1,3 @@
-#shell.nix
 with import <nixpkgs> {}; 
 
 pkgs.mkShell {
@@ -17,7 +16,11 @@ pkgs.mkShell {
 		arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 		arduino-cli core update-index
 		arduino-cli config set network.connection_timeout 300s
-		arduino-cli core install esp32:esp32
+		arduino-cli core install esp32:esp32@3.3.4
+		git clone https://github.com/ESP32Async/AsyncTCP ~/.arduino15/packages/esp32/hardware/esp32/3.3.4/libraries/AsyncTCP
+		git clone https://github.com/ESP32Async/ESPAsyncWebServer ~/.arduino15/packages/esp32/hardware/esp32/3.3.4/libraries/ESPAsyncWebServer
+		git clone https://github.com/hmueller01/pubsubclient3 ~/.arduino15/packages/esp32/hardware/esp32/3.3.4/libraries/pubsubclient3
+		
 		sudo chmod a+rw /dev/ttyUSB0
 		chmod +x Run.sh
 		echo "To compile and upload: ./Run.sh <sketch>"
